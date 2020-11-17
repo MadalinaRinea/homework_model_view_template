946cfcde4ce69e45e43b5ffe64d530a2f6740810
MadalinaRinea <66742630+MadalinaRinea@users.noreply.github.com>
2020-11-17 20:25:12 +0200

    Add files via upload

--- scripts/postDetails.js
+++ scripts/postDetails.js
@@ -1,11 +1,16 @@
 class PostDetailsView {
     postsModel = new PostsModel();
+    usersModel = new UsersModel();
+    commentsModel = new CommentsModel();
 
     constructor() {
         const id = this.getPostId();
         const post = this.postsModel.getPostById(id);
         this.hidrateHtml(post);
 
+        this.postComm = this.handleCommentFormSubmit;
+
+        document.querySelector('[data-comment-form]').addEventListener("submit", e => this.postComm(e));
         // attach event listener (submit event pentru adaugat comentarii)
     }
 
@@ -23,26 +28,55 @@ class PostDetailsView {
 
             titleElem.innerText = post.title;
             bodyElem.innerHTML = post.body;
-        })
+        });
     }
 
-    hidrateAuthor() {
+    hidrateAuthor(post) {
         // http://jsonplaceholder.typicode.com/users/1
-        // this.usersModel.getUserById(post.userId).then(user => {
-
-        // })
+        this.usersModel.getUserById(post.userId).then(user => {
+            const authorElem = document.querySelector('[data-post="author"]');
+            
+            authorElem.innerText = user.name;
+        });
     }
 
-    hidrateComments() {
+    hidrateComments(post) {
         // http://jsonplaceholder.typicode.com/posts/2/comments
-        //this.commentsModel.getCommentForPosts(2).then(comments => {
+        this.commentsModel.getCommentForPosts(2).then(comments => {
+            const fragment = document.createDocumentFragment();
 
-        // })
+            for (const comment of comments) {
+                
+                const i = document.createElement('i');
+                const h = document.createElement('h5');
+                const p = document.createElement('p');
+
+                h.innerText = comment.email + " :";
+                i.innerText ='"' +  comment.body + '"';
+
+                p.append(i);
+                fragment.append(h, p);
+            }
+            
+            this.appendComments(fragment)
+
+        });
     }
 
-    handlerCommentFormSubmit() {
+    appendComments(fragment) {
+        const comments = document.querySelector('[data-post="comments"]');
+
+        comments.append(fragment);
+    }
+
+    handlerCommentFormSubmit(e) {
+        e.preventDefault();
         //logica de add comment
-        // this.commentsModel.createComment(userId, commentBody)
+
+        const comment = document.querySelector("[data-comment]").value;
+        const email = "madalina.rinea@yahoo.com";
+
+        this.commentsModel.createComment(comment, email).then(console.log);
         // pentru aplicatia noastra vom transmite intotdeauna id 1 pentru user
     }
 }
--- scripts/users.model.js
+++ scripts/users.model.js
@@ -1,1 +1,9 @@
 // MVC Model View Controller
+
+class UsersModel {
+    apiUrl = 'http://jsonplaceholder.typicode.com/users/';
+
+    getUserById(id, options) {
+        return fetch(this.apiUrl + id, options).then(res => res.json());
+    }
+}
